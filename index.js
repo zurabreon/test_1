1fe8aaeb0e2c01183224f145523d87b72a9e4598
zurabreon <zguysanov.reon@gmail.com>
2023-12-11 15:42:57 +0300

    refactor
    Избавился от цикла, воспользовавшись готовым методом из utils, корректировал код

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const express = require("express");
 const api = require("./api");
 const logger = require("./logger");
 const config = require("./config");
+const utils = require ("./utils");
 
 const app = express();
 
@@ -14,63 +15,56 @@ app.use(express.urlencoded({ extended: true }));
 
 api.getAccessToken().then(() => {
 	
-	
-	app.get("/", (req, res) => res.send("123"));
-
 	app.get("/ping", (req, res) => res.send("pong " + Date.now()));
-
-
 
 	app.post("/hook", (req, res) => {
 		
-		var contactId = req.body.contacts.add[0].id;
-		var customerAge;
-		var birthdayId = 403675;
-		const currentData = new Date();
+		const contact_id = req.body.contacts.add[0].id;
+		const birthday_id = 467121; //id поля "День рождения"
+		const current_year = (new Date()).getFullYear();
+		const current_month = (new Date()).getMonth();
+		const current_day = (new Date()).getDate();
+		var customer_age;
+		
 
+		api.getContact(contact_id).then(response => {
 
-		api.getContact(contactId).then(response => {
-			for (i = 0; i < response.custom_fields_values.length; i++)
-			{
-				if (response.custom_fields_values[i].field_id === birthdayId)
-				{
-					const birthday = new Date(response.custom_fields_values[i].values[0].value * 1000);
+			const birthday_day = (new Date(utils.getFieldValue(response.custom_fields_values, birthday_id) * 1000)).getDate();
+			const birthday_month = (new Date(utils.getFieldValue(response.custom_fields_values, birthday_id) * 1000)).getMonth();
+			const birthday_year = (new Date(utils.getFieldValue(response.custom_fields_values, birthday_id) * 1000)).getFullYear();
 
-					if (currentData.getFullYear() - birthday.getFullYear() > 0) {
-						customerAge = currentData.getFullYear() - birthday.getFullYear();
-					}
-					else {
-						customerAge = 0;
-					}
-
-					if (currentData.getMonth() - birthday.getMonth() < 0 && customerAge != 0) {
-						customerAge--;
-					}
-					else {
-						if (currentData.getMonth() - birthday.getMonth() === 0 && currentData.getDate() - birthday.getDate() < 0 && customerAge != 0) {
-							customerAge--;
-						}
-					}
-					var customerAgeObject = {
-						field_id: 410341,
-						field_name: 'Возраст',
-						field_code: null,
-						field_type: 'numeric',
-						values: [
-							{
-								value: customerAge
-							}
-						],
-					};
-
-					response.custom_fields_values.push(customerAgeObject);
-
-					api.updateContacts(response);
-
-					logger.debug(response.custom_fields_values)
-					break;
+			if (current_year - birthday_year > 0) {
+					customer_age = current_year - birthday_year;
 				}
+			else {
+				customer_age = 0;
 			}
+
+			if (current_month - birthday_month < 0 && customer_age != 0) {
+						customer_age--;
+			}
+			else {
+					if (current_month - birthday_month === 0 && current_day - birthday_day < 0 && customer_age != 0) {
+							customer_age--;
+					}
+			}
+			const customer_age_object = {
+			field_id: 467125, // id поля "Возраст"
+			field_name: 'Возраст',
+			field_code: null,
+			field_type: 'numeric',
+			values: [
+					{
+						value: customer_age
+					}
+				],
+			};
+
+			response.custom_fields_values.push(customer_age_object);
+
+			api.updateContacts(response);
+
+			logger.debug(response.custom_fields_values)
 		})
 		
 		
